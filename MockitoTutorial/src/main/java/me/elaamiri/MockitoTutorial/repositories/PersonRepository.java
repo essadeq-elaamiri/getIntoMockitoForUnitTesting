dc460b9a285c6847e705b74bc27ee99f0e053528
essadeq-elaamiri <essadeq0701@gmail.com>
2025-01-04 22:05:15 +0100

    Repo tests

--- MockitoTutorial/src/main/java/me/elaamiri/MockitoTutorial/repositories/PersonRepository.java
+++ MockitoTutorial/src/main/java/me/elaamiri/MockitoTutorial/repositories/PersonRepository.java
@@ -4,6 +4,10 @@ import me.elaamiri.MockitoTutorial.entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> getPersonsByFirstNameContaining(String fnKeyword);
 }
--- MockitoTutorial/src/test/java/me/elaamiri/MockitoTutorial/RepositoriesTests/PersonRepositoryTest.java
+++ MockitoTutorial/src/test/java/me/elaamiri/MockitoTutorial/RepositoriesTests/PersonRepositoryTest.java
@@ -3,6 +3,7 @@ package me.elaamiri.MockitoTutorial.RepositoriesTests;
 import me.elaamiri.MockitoTutorial.entities.Person;
 import me.elaamiri.MockitoTutorial.repositories.PersonRepository;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.sql.Date;
+import java.util.List;
 
 
 @DataJpaTest
@@ -40,4 +42,112 @@ public class PersonRepositoryTest {
 
     }
 
+    @Test
+    public void PersonRepository_findAll_returnMoreThanOnePerson(){
+        // Arrange // prepare test data
+        Person person1 = Person.builder()
+                .firstName("person1fn")
+                .lastName("person1ln")
+                .birthDate(new Date(Date.valueOf("1999-01-07").getTime()))
+                .build();
+        Person person2 = Person.builder()
+                .firstName("person2fn")
+                .lastName("person2ln")
+                .birthDate(new Date(Date.valueOf("2004-01-07").getTime()))
+                .build();
+
+        personRepository.saveAll(Lists.list(person1, person2));
+        // Act // Perform test action
+
+        List<Person> personList = personRepository.findAll();
+
+        // Assert
+        //Assertions.assertThat(personList).isNotNull();
+        //Assertions.assertThat(personList).hasSize(2);
+        // We can use them in one chain
+        Assertions.assertThat(personList).isNotNull().hasSize(2);
+
+
+    }
+
+    @Test
+    public void PersonRepository_findById_returnCorrectPerson(){
+        // Arrange
+         Person person2 = Person.builder()
+                 .firstName("person2fn")
+                 .lastName("person2ln")
+                 .birthDate(new Date(Date.valueOf("2004-01-07").getTime()))
+                 .build();
+
+         personRepository.save(person2); // person2 will get its id
+
+         // Act
+         Person retrievedPerson = personRepository.findById(person2.getId()).get();
+
+         // Assert
+         Assertions.assertThat(person2).isNotNull().isEqualTo(retrievedPerson);
+    }
+
+    @Test
+    public void PersonRepository_getPersonByFirstNameContaining_returnListPersonWithFirstNameContainingKeyword(){
+        // Arrange // prepare test data
+        Person person1 = Person.builder()
+                .firstName("person1fn")
+                .lastName("person1ln")
+                .birthDate(new Date(Date.valueOf("1999-01-07").getTime()))
+                .build();
+        Person person2 = Person.builder()
+                .firstName("person2fn")
+                .lastName("person2ln")
+                .birthDate(new Date(Date.valueOf("2004-01-07").getTime()))
+                .build();
+
+        Person person3 = Person.builder()
+                .firstName("differentFirstName")
+                .lastName("person2ln")
+                .birthDate(new Date(Date.valueOf("2004-01-07").getTime()))
+                .build();
+
+        personRepository.saveAll(Lists.list(person1, person2, person3));
+        // Act // Perform test action
+
+        List<Person> personList = personRepository.getPersonsByFirstNameContaining("per");
+
+        // Assert
+        Assertions.assertThat(personList).isNotNull().hasSize(2);
+    }
+
+    @Test
+    public void PersonRepository_update_returnPersonNotNull(){
+        Person person1 = Person.builder()
+                .firstName("person1fn")
+                .lastName("person1ln")
+                .birthDate(new Date(Date.valueOf("1999-01-07").getTime()))
+                .build();
+        personRepository.save(person1);
+        Person personFromDb = personRepository.findById(person1.getId()).get();
+        personFromDb.setFirstName("differentName");
+
+        Person savedAfterUpdate = personRepository.save(personFromDb);
+
+        Assertions.assertThat(savedAfterUpdate).isNotNull().isEqualTo(personFromDb);
+
+    }
+
+    @Test
+    public void PersonRepository_delete_returnPersonNull(){
+        Person person1 = Person.builder()
+                .firstName("person1fn")
+                .lastName("person1ln")
+                .birthDate(new Date(Date.valueOf("1999-01-07").getTime()))
+                .build();
+
+        personRepository.save(person1);
+
+        personRepository.deleteById(person1.getId());
+
+        Person personFromDb = personRepository.findById(person1.getId()).orElse(null);
+
+        Assertions.assertThat(personFromDb).isNull();
+    }
 }
